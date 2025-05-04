2fbedc5bbd5c585074f46bdb25a4d9664bbfdad1
pipoy <94201428+teknotranz@users.noreply.github.com>
2025-05-04 01:21:16 -0400

    t

--- DSA-JAVA/src/DSA.java
+++ DSA-JAVA/src/DSA.java
@@ -5,14 +5,11 @@ public class DSA {
 
     public void bubbleSort(){
 
-        //1. start doing 1 loop where it puts the highest value of the array at the end through swapping
+        //1. start doing 1 pass where it puts the highest value of the array at the end through swapping
         //1a the result of first loop will result: 7-3-4-6-8
         //1b now we just need to loop this n times by adding an outer loop that will loop n-times
 
-        //improvement is in the inner loop we made it arr.length - i -i
-        //when we make the first pass, we know that the highest value is in the last element
-        //at first loop we only loop up to n-1, second loop up to n-2
-        //we use the i, outerloop index, to subtract
+
         for(int i = 0; i < arr.length; i++){
             for(int j = 0; j < arr.length-i-1; j++){
                 if(arr[j] > arr[j+1]){
@@ -22,6 +19,12 @@ public class DSA {
                 }
             }
         }
+
+        //improvement is in the inner loop we made it arr.length - i -i
+        //when we make the first pass, we know that the highest value is in the last element
+        //at first loop we only loop up to n-1, second loop up to n-2
+        //we use the i, outerloop index, to subtract
+
         System.out.println(Arrays.toString(arr));
     }
 }
---
+++ DSA-JAVA/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+        // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+        DSA dsa = new DSA();
+        dsa.bubbleSort();
+
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+
+    }
+}
